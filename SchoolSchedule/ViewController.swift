b2a3a492595999e452345467585c34d818092482
Yu <iaag@ezweb.ne.jp>
2021-02-20 14:46:42 +0900

    Initial Commit

---
+++ SchoolSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchoolSchedule
+//
+//  Created by 箭内雄 on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
